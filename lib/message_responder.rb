595c29a26425bcd241dfc4e15149de8a12fdb142
Alexey Nikolaev <bitofuniverse@gmail.com>
2016-05-01 22:51:39 +0300

    Refactor #respond method

--- lib/message_responder.rb
+++ lib/message_responder.rb
@@ -22,20 +22,20 @@ class MessageResponder
 
     text = case parse_command(message.text)
              when '/start'
-               answer_with_greeting_message
+               greeting_message
              when '/stop'
-               answer_with_farewell_message
+               farewell_message
              when '/idea'
-               answer_with_ideas(1)
+               collect_ideas(1)
              when '/5ideas'
-               answer_with_ideas(5)
+               collect_ideas(5)
              when '/10ideas'
-               answer_with_ideas(10)
+               collect_ideas(10)
              else
-               answer_with_text message.text
+               message.text # echo
            end
-    
-    logger.debug "respond: #{text}"
+
+    answer_with_text text
   end
 
   private
@@ -44,7 +44,7 @@ class MessageResponder
     text.split(bot_username).last.strip
   end
 
-  def answer_with_ideas(num)
+  def collect_ideas(num)
     ideas = load_ideas(num)
 
     ideas.map do |idea|
@@ -68,15 +68,11 @@ class MessageResponder
     MessageSender.new(bot: bot, chat: message.chat, text: text).send
   end
 
-  def answer_with_greeting_message
-    text = I18n.t('greeting_message')
-
-    answer_with_text(text)
+  def greeting_message
+    I18n.t('greeting_message')
   end
 
-  def answer_with_farewell_message
-    text = I18n.t('farewell_message')
-
-    answer_with_text(text)
+  def farewell_message
+    I18n.t('farewell_message')
   end
 end
